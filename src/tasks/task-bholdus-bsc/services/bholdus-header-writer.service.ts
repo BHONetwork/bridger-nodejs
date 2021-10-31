fee778a44cb00defd1f5edaafa4f88b4f930b37d
Daniel Lam <lhdung258@gmail.com>
2021-10-31 22:04:21 +0700

    add bholdus deps

--- src/tasks/task-bholdus-bsc/services/bholdus-header-writer.service.ts
+++ src/tasks/task-bholdus-bsc/services/bholdus-header-writer.service.ts
@@ -1,11 +1,17 @@
 import { inject, injectable } from 'inversify';
 import { IService } from '@lib/task';
 import { BholdusBscTaskBus } from '../bus';
+import { ApiPromise } from '@polkadot/api';
+import getApiOptions from '@bholdus/api-options';
 
 @injectable()
 export class BholdusHeaderWriterService implements IService<BholdusBscTaskBus> {
 	static symbol = Symbol(BholdusHeaderWriterService.name);
 	@inject(BholdusBscTaskBus.symbol) private bus: BholdusBscTaskBus;
+	constructor() {}
 
-	async start(): Promise<void> {}
+	async start(): Promise<void> {
+		const api = await ApiPromise.create(getApiOptions());
+	
+	}
 }
