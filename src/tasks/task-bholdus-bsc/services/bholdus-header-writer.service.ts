af4978b3cb01214838d345e3f989f2f8e56009f8
bholdus-Pham-Dat <dat.pham@bholdus.com>
2021-11-04 14:36:22 +0700

    update return response

--- src/tasks/task-bholdus-bsc/services/bholdus-header-writer.service.ts
+++ src/tasks/task-bholdus-bsc/services/bholdus-header-writer.service.ts
@@ -1,12 +1,11 @@
 import { inject, injectable } from 'inversify';
 import { IService } from '@lib/task';
 import { BholdusBscTaskBus } from '../bus';
-import { ApiPromise, WsProvider} from '@polkadot/api';
+import { ApiPromise, WsProvider, Keyring} from '@polkadot/api';
 import getApiOptions from '@bholdus/api-options';
-import {BholdusWriterMessage} from '../message'
+import {BholdusWriterSubmitBscHeaderMessage} from '../message'
 import { BscPrimitivesBscHeader } from '@bholdus/types/interfaces/bscPrimitives';
-
-
+import logger from '@lib/logger';
 @injectable()
 export class BholdusHeaderWriterService implements IService<BholdusBscTaskBus> {
 	static symbol = Symbol(BholdusHeaderWriterService.name);
@@ -14,15 +13,25 @@ export class BholdusHeaderWriterService implements IService<BholdusBscTaskBus> {
 	constructor() {}
 
 	async start(): Promise<void> {
+		logger.info(BholdusHeaderWriterService.name);
 		// const api = await ApiPromise.create(getApiOptions());
 		const wsProvider = new WsProvider('ws://127.0.0.1:9944');
 		const api = await ApiPromise.create({ provider: wsProvider });
+		const keyring = new Keyring({ type: 'sr25519' });
+		const alice = keyring.addFromUri('//Alice');
 
 		// Do something
-		this.bus.channel(BholdusWriterMessage).subscribe({next:(message: BscPrimitivesBscHeader[]) =>{
+		this.bus.channel(BholdusWriterSubmitBscHeaderMessage).subscribe({next:(message: BscPrimitivesBscHeader[]) =>{
 			let req = api.tx.bsc.verifyAndUpdateAuthoritySetSigned(message)
-			console.log("Result writer: ", req)
-			console.log("Message headerbsc: ", message)
+			req.signAndSend(alice, (resp) => {
+				logger.info(BholdusHeaderWriterService.name + " - status: " + resp.status);
+
+				if (resp.status.isInBlock) {
+					logger.info(BholdusHeaderWriterService.name + ` - Transaction included at blockHash: ${resp.status.asInBlock}`);
+				  } else if (resp.status.isFinalized) {
+					logger.info(BholdusHeaderWriterService.name + ` - Transaction finalized at blockHash: ${resp.status.asFinalized}`);
+				  }
+			})
 		}})
 	}
 }
